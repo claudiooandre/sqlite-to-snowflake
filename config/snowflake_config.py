12536595e31f3e910a5d099c2c255070785a9c28
claudioo.andre <72884870+claudiooandre@users.noreply.github.com>
2025-06-15 20:09:17 +0100

    Update snowflake_config.py

--- config/snowflake_config.py
+++ config/snowflake_config.py
@@ -1,8 +1,8 @@
 # ⚠️ Substitua os valores abaixo pelas suas credenciais reais do Snowflake
 conn_params = {
-    'user': 'claudiorodrigues90',
-    'password': 'pyhCeqnumjewnivfi7',
-    'account': 'xlsvnwt-ak53705',  # Ex: xy12345.eu-west-1
+    'user': '<user>',
+    'password': '<password>',
+    'account': '<account>',  # Ex: xy12345.eu-west-1
     'warehouse': 'SNOWFLAKE_LEARNING_WH',
     'database': 'SNOWFLAKE_LEARNING_DB',
     'schema': 'CLAUDIORODRIGUES90_LOAD_SAMPLE_DATA_FROM_S3'
